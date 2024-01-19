07d794b9b1da4a96ceee5b47f21bf6053e349d4c
Amarjeet <kumaramarjeet001@gmail.com>
2024-01-19 12:52:06 +0530

    ci(entity) : integrate rating entity to collect user ratings into an array

---
+++ usermanagement/src/main/java/com/user/usermanagement/entities/Rating.java
@@ -0,0 +1,16 @@
+package com.user.usermanagement.entities;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Rating {
+    
+    private String ratingId ;
+    private String userId ;
+    private String hotelId ;
+    private String feedback ;
+    private int rating ;
+}
--- usermanagement/src/main/java/com/user/usermanagement/entities/User.java
+++ usermanagement/src/main/java/com/user/usermanagement/entities/User.java
@@ -1,8 +1,12 @@
 package com.user.usermanagement.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -21,5 +25,7 @@ public class User {
     private String name ;
     private boolean is_active ;
     private String city ;
+    @Transient
+    private List<Rating> ratings = new ArrayList<>() ;
 
 }
