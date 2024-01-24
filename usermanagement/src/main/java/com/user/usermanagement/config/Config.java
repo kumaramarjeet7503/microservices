2970a3b128b60e0dd0ed44110478844e051c0677
Amarjeet <kumaramarjeet001@gmail.com>
2024-01-24 23:42:46 +0530

    * fix(fixed values) : remove url hardcoding , replace with service name

--- usermanagement/src/main/java/com/user/usermanagement/config/Config.java
+++ usermanagement/src/main/java/com/user/usermanagement/config/Config.java
@@ -1,5 +1,6 @@
 package com.user.usermanagement.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
@@ -9,6 +10,7 @@ import org.springframework.web.client.RestTemplate;
 public class Config {
 
     @Bean
+    @LoadBalanced
      RestTemplate restTemplate()
     {
 
--- usermanagement/src/main/java/com/user/usermanagement/service/UserService.java
+++ usermanagement/src/main/java/com/user/usermanagement/service/UserService.java
@@ -3,6 +3,7 @@ package com.user.usermanagement.service;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
@@ -32,26 +33,22 @@ public class UserService {
     private Utils utils ;
  
     public List<User> getAllUsers()
-    {
-        return   this.userRepo.findAll() ;
+    {           
+        List<User> users =  this.userRepo.findAll() ;
+        List<User> ratedUser = users.stream().map(user -> {
+            List<Rating> ratings = setRatingFromResponse(user.getId()) ;
+            user.setRatings(ratings);
+            return user ;
+        }).collect(Collectors.toList()) ;
+        return ratedUser ;
     }
 
     public User getUser(String userId)
     {
         User user = this.userRepo.findById(userId).orElseThrow(()-> new ResourceNotFoundException("Please enter a valid user id"))  ; 
-        String ratingUrl = "http://localhost:8092/user/get-rating/"+userId ;
-        Rating[] rating = this.restTemplate.getForObject(ratingUrl, Rating[].class);
-        
-         List<Rating> ratings = Arrays.stream(rating).toList() ;
-        String hotelUrl = "http://localhost:8093/get-hotel/" ;
-         List<Rating> hotelRating = ratings.stream().map(ratingObj -> {
-                ResponseEntity<Hotel> responseHotel = this.restTemplate.getForEntity(hotelUrl+ratingObj.getHotelId() ,Hotel.class) ;
-                Hotel hotel = responseHotel.getBody() ;
-                ratingObj.setHotel(hotel);
-                return ratingObj ;
-         }).collect(Collectors.toList())  ;
-
-        user.setRatings(hotelRating);
+     
+        List<Rating> ratings = setRatingFromResponse(userId) ;
+        user.setRatings(ratings);
         return  user  ;
     }
 
@@ -60,5 +57,22 @@ public class UserService {
         user.setId(utils.generateRandomId());
         User newUser = this.userRepo.save(user) ;
         return  newUser ;
+    }
+
+    private List<Rating> setRatingFromResponse(String userId)
+    {
+        String ratingUrl = "http://RATING-MANAGEMENT/user/get-rating/"+userId ;
+        Rating[] rating = this.restTemplate.getForObject(ratingUrl, Rating[].class);
+        
+         List<Rating> ratings = Arrays.stream(rating).toList() ;
+        String hotelUrl = "http://HOTEL-MANAGEMENT/get-hotel/" ;
+         List<Rating> hotelRating = ratings.stream().map(ratingObj -> {
+                ResponseEntity<Hotel> responseHotel = this.restTemplate.getForEntity(hotelUrl+ratingObj.getHotelId() ,Hotel.class) ;
+                Hotel hotel = responseHotel.getBody() ;
+                ratingObj.setHotel(hotel);
+                return ratingObj ;
+         }).collect(Collectors.toList())  ;
+
+         return hotelRating ;
     }
 }
